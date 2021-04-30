b334fda7052dba19ad9f00ea655f63439c21ac03
Marko Vasic <marko.vasic@nfcsb.com>
2021-04-30 13:27:26 +0200

    Add home page

--- VocabularyBooster/ClientApp/src/app/app.module.ts
+++ VocabularyBooster/ClientApp/src/app/app.module.ts
@@ -6,15 +6,16 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
+
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HomeModule } from './pages/home/home.module';
+import { HomePageComponent } from './pages/home/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
     CounterComponent,
     FetchDataComponent
   ],
@@ -23,10 +24,11 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: 'index', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: HomePageComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
