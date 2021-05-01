e9cb867f9344ed4a886c4167f575ece9f746ffea
Marko Vasic <marko.vasic@nfcsb.com>
2021-05-01 11:54:20 +0200

    Add word-edit component

--- VocabularyBooster/ClientApp/src/app/components/components.module.ts
+++ VocabularyBooster/ClientApp/src/app/components/components.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WordComponent } from './word/word.component';
+import { WordEditComponent } from './word-edit/word-edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [WordComponent],
+  declarations: [WordComponent, WordEditComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
-    WordComponent
+    WordComponent,
+    WordEditComponent
   ]
 })
 export class ComponentsModule { }
---
+++ VocabularyBooster/ClientApp/src/app/components/word-edit/word-edit.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Word } from 'src/app/data/api/models';
+import { WordService } from 'src/app/data/api/services';
+
+@Component({
+  selector: 'vb-word-edit',
+  templateUrl: './word-edit.component.html',
+  styleUrls: ['./word-edit.component.css']
+})
+export class WordEditComponent implements OnInit {
+  @Input('word') word: Word;
+
+  form: FormGroup;
+
+  constructor(
+    private wordService: WordService
+  ) {
+  }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      expression: new FormControl("", Validators.minLength(1)),
+      definition: new FormControl("", Validators.minLength(1)),
+      example: new FormControl("", Validators.minLength(1)),
+      grammaticalCategories: new FormControl("", Validators.minLength(1)),
+      thesaurus: new FormControl(""),
+      callocations: new FormControl(""),
+      cefr: new FormControl(""),
+      topic: new FormControl(""),
+    });
+  }
+
+  submit() {
+    console.log(this.form.getRawValue().definition.trim())
+    this.wordService.AddWord({
+      body: {
+        expression: this.form.getRawValue().expression.trim(),
+        sense: [
+          {
+            definition: this.form.getRawValue().definition.trim(),
+            example: this.form.getRawValue().example.trim(),
+            grammaticalCategories: this.form.getRawValue().grammaticalCategories.trim(),
+            thesaurus: this.form.getRawValue().thesaurus.trim(),
+            callocations: this.form.getRawValue().callocations.trim(),
+            cefr: this.form.getRawValue().cefr.trim(),
+            topic: this.form.getRawValue().topic.trim(),
+          }
+        ]
+      }
+    }).subscribe();
+  }
+}
